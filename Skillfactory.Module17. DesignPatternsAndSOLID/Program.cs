e79305c20b5e0f8a4835d8be2b631f756bce2b1b
Anatolii Ivanov <gotnickname@gmail.com>
2023-06-17 11:06:56 +0600

    Add project files.

---
+++ Skillfactory.Module17. DesignPatternsAndSOLID/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Skillfactory.Module17._DesignPatternsAndSOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Some example code.
+            Account testAccount = new Account() { Type = "Обычный", Balance = 100, Interest = 40 };
+            Calculator.CalculateInterest(testAccount);
+            Console.WriteLine(testAccount.Interest);
+
+            Console.ReadKey();
+        }
+    }
+}
